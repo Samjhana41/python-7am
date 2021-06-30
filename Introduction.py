96131bfb4e235a0e4a6fc381d8d06321fa155145
Samjhana Khanal <samjhanakhanal04@gmail.com>
2021-06-30 16:29:17 +0200

    my first commit

---
+++ Introduction.py
@@ -0,0 +1,11 @@
+print('hello python update')
+print("hello")
+fName = "ram prasad"
+LName = "abc"
+age = 20
+print(f"My name is :{fName} {LName} Age:{age}")
+X= 10
+print(type(X))
+x=input("Enter any number:")
+print(x)
+

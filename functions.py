587f4f49c6194d993ca077e12a78eb6d05fa87d2
Samjhana Khanal <samjhanakhanal04@gmail.com>
2021-07-15 17:19:40 +0200

    functions.py

--- functions.py
+++ functions.py
@@ -1,11 +1,13 @@
-print()
+#print()
 
-def message():
-    print("Hello python")
-
-    def introduction(name, age=0):
-        print(name)
-        print(age)
-
-
-message()
+def interest(p,time=2,rate=0.10):
+    return p*time*rate
+p=float(input("enter principle amount: "))
+x=interest(p)
+print("rs.",x)
+print("si with defaulter rate and time")
+r=float(input("enter rate of interest:"))
+t=float(input("enter time period:"))
+y=interest(p,t,r)
+print("rs", y)
+print("si with mordified rate and time")
